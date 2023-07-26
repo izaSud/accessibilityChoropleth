cd80328b8eee46f923cd9dd02c8e52c72578f656
Izabela <izabelasudac@gmail.com>
2023-07-26 18:19:18 +0200

    Fixing build.js

--- build.js
+++ build.js
@@ -2,7 +2,7 @@
 const mapboxToken = process.argv[2]; // Read the first command-line argument
 
 // Store the mapboxToken variable on the window object to make it accessible globally
-window.mapboxToken = mapboxToken;
+module.mapboxToken = mapboxToken;
 
 // Use the 'mapboxToken' variable in your build process or pass it to your frontend code
 console.log('Mapbox Token:', mapboxToken);
